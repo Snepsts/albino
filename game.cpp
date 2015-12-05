a859c650cf42fda664af8da0dc0a9abc959f7576
rancigliom <rancigliom@gmail.com>
2015-12-05 13:35:03 -0500

    Make game.cpp playable/testable
    First implementation of a game-like atmosphere. Will also be used to test if the player class functions work. So expect to see fixes/improvements in those soon.

--- game.cpp
+++ game.cpp
@@ -5,11 +5,92 @@
 
 using namespace std;
 
+void display_choices();
+
 int main()
 {
 	srand(time(0)); //Ensure number is random everytime
 	
-	cout << "Sorry, but the main function has been cleaned. There is no implementation of the class player yet." << endl;
+	player p1;
+	int choicevar;
+	bool whilevar;
+	
+	cout << "What would you like to do?" << endl;
+	cout << "List:" << endl;
+	cout << "1 - New Game" << endl;
+	cout << "2 - Load Game" << endl;
+	cout << "3 - Exit Game" << endl;
+	
+	do
+	{
+		cin >> choicevar;
+		
+		switch(choicevar)
+		{
+			case 1:
+				p1.choose_class();
+				whilevar = false;
+				break;
+				
+			case 2:
+				p1.load_game();
+				whilevar = false;
+				break;
+				
+			case 3:
+				cout << "Thank you for playing!" << endl;
+				whilevar = false;
+				exit(0);
+				break;
+				
+			default:
+				cout << "Not a valid option." << endl;
+				whilevar = true;
+				break;
+		}
+	}while(whilevar);
+	
+	cout << "What would you like to do now?" << endl;
+	display_choices();
+	
+	do
+	{
+		cin >> choicevar;
+		
+		switch(choicevar)
+		{
+			case 1:
+				p1.save_game();
+				whilevar = true;
+				break;
+				
+			case 2:
+				p1.load_game();
+				whilevar = true;
+				break;
+				
+			case 3:
+				p1.display();
+				whilevar = true;
+				break;
+				
+			case 4:
+				cout << "Thanks for playing!" << endl;
+				whilevar = false;
+				break;
+		}
+	}while(whilevar);
+	
+	cout << "There will be more soon!" << endl;
 	
 	return 0;
+}
+
+void display_choices()
+{
+	cout << "List:" << endl;
+	cout << "1 - New Game" << endl;
+	cout << "2 - Load Game" << endl;
+	cout << "3 - Display Chocies" << endl;
+	cout << "4 - Exit Game" << endl;
 }
