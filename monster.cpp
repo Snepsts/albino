76d29a481825f1afb190eceda8a02a649a65f3e0
Snepsts <rancigliom@gmail.com>
2015-12-07 13:30:32 -0600

    Add monster class
    Initial monster class commit. I haven't added it to the game.cpp or makefile yet because there's more stuff to do before then.

---
+++ monster.cpp
@@ -0,0 +1,32 @@
+#include "monster.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+monster::monster()
+{
+	health = 10; defense = 3; strength = 3; speed = 3; level = 0;
+}
+
+void monster::spawnmonster(int lvl)
+{
+	srand(time(0)); //Ensure number is random everytime
+	int lvlvar, w, x, y, z;
+	w = rand() % 5;
+	x = rand() % 3;
+	y = rand() % 3;
+	z = rand() % 3;
+	health = health + w + 1;
+	strength = strength + x + 1;
+	defense = defense + y + 1;
+	speed = speed + z + 1;
+	lvlvar = strength + defense + speed;
+	lvlvar/12 = level;
+	displaymonster();
+}
+
+void monster::displaymonster()
+{
+	cout << "You have encountered a monster that is level " << level << "!" << endl;
+}
---
+++ monster.h
@@ -0,0 +1,22 @@
+/* Currently the enemy or thing the player fights is called "monster" this is
+subject to change in the future, but for now here's the definition */
+
+#ifndef __MONSTER_H__
+#define __MONSTER_H__
+
+class monster
+{
+public:
+	monster();
+	void spawnmonster(int lvl);
+	void displaymonster();
+
+private:
+	int health;
+	int strength;
+	int defense;
+	int speed;
+	int level;
+}
+
+#endif
