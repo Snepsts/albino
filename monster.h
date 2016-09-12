49eed966bd5a25b2e3fee9f03a78dd7314551dec
Snepsts <rancigliom@gmail.com>
2016-09-12 13:28:57 -0500

    Move monster documentation from the cpp to the header
    Also add a little extra documentation.

--- monster.h
+++ monster.h
@@ -12,11 +12,41 @@ class player;
 class monster
 {
 public:
+	/* default constructor
+		Task: To create the "monster" object. Default values are set here and
+		can be adjusted as seen fit. */
 	monster();
+
+	/* function spawnmonster
+		Task: To decide the values to add to the stats. Can and will be adjusted
+		to scale with player level, but that'll be later. (I sure do say that often)
+
+		This function takes some rand stuff and adds them to the base stats.
+		Right now it uses w, x, y, and z. Those correspond to health, strength,
+		defense, and speed respectively. I'm not really sure why I didn't use a
+		b c etc instead since that is more flexible but whatever.
+
+		It also sets the hp to the health max since there's not really a better
+		way to implement that atm. Also it calls displaymonster at the end. Since
+		for now that's the only time we'll use it anyway is after a spawn. */
 	void spawnmonster(int lvl);
+
+	/* Displaymonster
+		For now this is the text that introduces the monster. However, I will
+		change this later as well to be a stat display probably... maybe. */
 	void displaymonster();
+
+	/* isAlive
+		Very basic bool, but makes for an easy "ensure all entities are alive"
+		check */
 	bool isAlive();
+
+	/* ai_choice
+		Not done yet, I have to fix my formatting for all the class .cpp
+		documentation anyway... //do later */
 	action ai_choice();
+
+	//Friend functions for battle
 	friend void battle(player& p1);
 	friend void damage(action act, player& p1, monster& m1);
 	friend void dmodify(action act, player& p1, monster& m1);
