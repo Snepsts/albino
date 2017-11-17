28e0c4bfaaf6ae242aaa6155144d8dd51abed9b5
Michael Ranciglio <rancigliom@gmail.com>
2017-11-17 15:31:14 -0600

    Add license

--- src/game.cpp
+++ src/game.cpp
@@ -1,3 +1,19 @@
+/* albino
+Copyright (C) 2017 Michael Ranciglio
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include <iostream> //cin & cout
 #include <cstdlib> //srand() and rand()
 #include <ctime> //time(0)
@@ -29,8 +45,7 @@ int main()
 	printw("There are %d rows and %d cols", row, col);
 	refresh();
 
-	if (true) //do this so destructor is called at the end of the if
-	{
+	if (true) { //do this so destructor is called at the end of the if
 		window *player_info = new window(5, 23, 3, 0, 7);
 		window *maze = new window(12, 23, 8, 0, 5);
 		window *text_log = new window(12, 23, 3, 24, 7);
