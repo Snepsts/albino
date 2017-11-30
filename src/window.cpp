a80db8c9fd77fc56ce726f7cb0322fe97a6ce5af
Snepsts <rancigliom@gmail.com>
2017-11-30 12:51:51 -0600

    Remove custom cstring method for builtin one
    This also fixes a bug where the test string sometimes has random characters attached to it.

--- src/window.cpp
+++ src/window.cpp
@@ -19,16 +19,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
 #include "window.h"
 
-char* string_to_char_array(const std::string &s)
-{
-	char* c = new char[s.length()]; //allocate memory
-
-	for (uint i = 0; i < s.length(); i++)
-		c[i] = s[i]; //assign string to char array
-
-	return c;
-}
-
 window::window(const int &height, const int &width, const int &starty, const int &startx, const int &color)
 {
 	win = newwin(height, width, starty, startx); //make a new window
@@ -50,12 +40,7 @@ window::~window()
 void window::print(const std::string &s, const int &y, const int &x)
 {
 	//wprintw takes a WINDOW* and a const char* argument
-	const char *msg = string_to_char_array(s); //turn string to char array
-
-	mvwprintw(win, y, x, msg); //print the message
-
-	delete[] msg; //clear memory
-	msg = nullptr; //safely set msg to point at nothing
+	mvwprintw(win, y, x, s.c_str()); //print the message
 
 	wrefresh(win);
 }
