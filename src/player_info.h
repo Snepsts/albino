5fe6726664bf653da6c66e1357d2088c86f7d923
Snepsts <rancigliom@gmail.com>
2017-12-01 09:41:55 -0600

    Added initial player_info class files
    The player_info class will have a pointer to the player and display important
    information to the user like player health, attack stats, etc. These are the
    first files for it.

---
+++ src/player_info.h
@@ -0,0 +1,36 @@
+/* albino
+Copyright (C) 2017 Michael Ranciglio
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef __PLAYER_INFO_H__
+#define __PLAYER_INFO_H__
+
+#include <string>
+
+#include "player.h"
+#include "window.h"
+
+class player_info
+{
+public:
+	player_info(player* playa);
+	~player_info();
+	void print(const std::string &s);
+private:
+	window *win;
+	player *p;
+};
+
+#endif //__PLAYER_INFO_H__
