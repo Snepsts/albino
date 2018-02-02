e2882956d7776f3118734ce6cb0e2cb78d7fd6a7
Michael Ranciglio <rancigliom@gmail.com>
2018-02-01 18:38:36 -0600

    Add new base class
    The purpose of this class is to be a "base" for detailed things in the game.
    I.e: attacks, items, and other stuff.

---
+++ src/base.h
@@ -0,0 +1,35 @@
+/* albino
+Copyright (C) 2018 Michael Ranciglio
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
+//base class for items, attacks, and maybe other stuff in the future
+
+#ifndef __BASE_H__
+#define __BASE_H__
+
+#include <string>
+
+class base
+{
+public:
+	base() { name = "null"; desc = "null"; }
+	virtual std::string to_string() { return name + ":\n" + desc; }
+	virtual std::string get_title() { return "base"; }
+private:
+	std::string name;
+	std::string desc;
+};
+
+#endif //__BASE_H__
