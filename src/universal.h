0ef593f8490a336230cdf6fd227781755723d67d
Michael Ranciglio <rancigliom@gmail.com>
2018-02-07 00:51:14 -0600

    Add DSW_HEIGHT and change all ints to uints in universal.h

--- src/universal.h
+++ src/universal.h
@@ -22,33 +22,34 @@ along with this program. If not, see <http://www.gnu.org/licenses/>. */
 const std::string VERSION = "0.0.1";
 
 //window constants
-const int Y_OFFSET = 3;
+const uint Y_OFFSET = 3;
 
 //player_info constants
-const int PI_HEIGHT = 5;
-const int PI_WIDTH = 24;
-const int PI_STARTY = Y_OFFSET;
-const int PI_STARTX = 0;
-const int PI_COLOR = 5;
+const uint PI_HEIGHT = 5;
+const uint PI_WIDTH = 24;
+const uint PI_STARTY = Y_OFFSET;
+const uint PI_STARTX = 0;
+const uint PI_COLOR = 5;
 
 //maze_window constants
-const int MW_HEIGHT = 13;
-const int MW_WIDTH = PI_WIDTH;
-const int MW_STARTY = Y_OFFSET + PI_HEIGHT;
-const int MW_STARTX = PI_STARTX;
-const int MW_COLOR = 5;
+const uint MW_HEIGHT = 13;
+const uint MW_WIDTH = PI_WIDTH;
+const uint MW_STARTY = Y_OFFSET + PI_HEIGHT;
+const uint MW_STARTX = PI_STARTX;
+const uint MW_COLOR = 5;
 
 //text_log constants
-const int TL_STARTY = Y_OFFSET;
-const int TL_STARTX = PI_WIDTH + 1;
-const int TL_COLOR = 5;
+const uint TL_STARTY = Y_OFFSET;
+const uint TL_STARTX = PI_WIDTH + 1;
+const uint TL_COLOR = 5;
 
 //gettext_window constants
-const int GTW_HEIGHT = 8;
-const int GTW_WIDTH = 22;
-const int GTW_COLOR = 1;
+const uint GTW_HEIGHT = 8;
+const uint GTW_WIDTH = 22;
+const uint GTW_COLOR = 1;
 
 //detailed_selection_window constants
-const int DSW_HEIGHT = 10;
+const uint DSW_HEIGHT = 10;
+const uint DSW_WIDTH = 26;
 
 #endif //__UNIVERSAL_H__
